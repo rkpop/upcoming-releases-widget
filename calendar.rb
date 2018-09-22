22a51da383797c2a6e311f5ed8dcdabf4e29cc63
Jonathan Nicholas <jonicrecis@gmail.com>
2018-09-23 02:14:54 +1000

    calendar is now a class

--- calendar.rb
+++ calendar.rb
@@ -1,34 +1,58 @@
+# frozen_string_literal: true
+
 require 'google_calendar'
 require_relative 'time_wrapper'
+require_relative 'database'
 require 'time'
 
-def generate_calendar(entry)
-  title = "#{entry[:artist_name]} - #{entry[:album_title]}"
-  month_year = current_time
-  start_time = Time.new(month_year[:year],
-                        month_year[:month],
-                        entry[:release_date],
-                        entry[:release_time][:hour],
-                        entry[:release_time][:minute],
-                        0,
-                        '+09:00')
-  end_time = start_time + 3600
-  final_structure = { title: title,
-                      start_time: start_time,
-                      end_time: end_time,
-                      description: entry[:album_type] }
-  final_structure
-end
+class Calendar
+  def initialize(calendar_instance, database_instance)
+    @calendar = calendar_instance
+    @database = database_instance
+  end
 
-def upload(calendar, data)
-  data.each do |entry|
-    proper_entry = generate_calendar(entry)
-    event_id = calendar.create_event do |e|
+  def add_entry(data)
+    data.each do |entry|
+      proper_entry = generate_calendar(entry)
+      cal_id = @database.check_entry(proper_entry[:title])
+      if cal_id.nil? then upload(proper_entry) else update(cal_id, proper_entry)
+      end
+    end
+  end
+
+  private
+
+  def generate_calendar(entry)
+    title = "#{entry[:artist_name]} - #{entry[:album_title]}"
+    start_time = Time.new(current_time[:year],
+                          current_time[:month],
+                          entry[:release_date],
+                          entry[:release_time][:hour],
+                          entry[:release_time][:minute],
+                          0,
+                          '+09:00')
+    final_structure = { title: title,
+                        start_time: start_time,
+                        end_time: start_time + 3600,
+                        description: entry[:album_type] }
+    final_structure
+  end
+
+  def upload(proper_entry)
+    event_id = @calendar.create_event do |e|
       e.title = proper_entry[:title]
       e.start_time = proper_entry[:start_time]
       e.end_time = proper_entry[:end_time]
       e.description = proper_entry[:description]
     end
-    puts event_id
+    database_input = { cal_id: event_id.id, title: proper_entry[:title] }
+    @database.add_entry(database_input)
+  end
+
+  def update(id, proper_entry)
+    @calendar.find_or_create_event_by_id(id) do |e|
+      e.start_time = proper_entry[:start_time]
+      e.end_time = proper_entry[:end_time]
+    end
   end
 end
